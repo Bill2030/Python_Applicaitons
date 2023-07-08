1560852a4d6948c41fd993b4339b1218269174b0
Bill2030 <benardbiidocuments@gmail.com>
2023-07-08 11:02:18 +0300

    Continuation of Python Programming

--- Day 63/main.py
+++ Day 63/main.py
@@ -1,79 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import csv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
 
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), Movie())
-    rating = db.Column(db.Float, nullable=False)
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-    def __init__(self, id, title, author, rating):
-        self.id = id
-        self.title = title
-        self.author = author
-        self.rating = rating
-
-    def __repr__(self):
-        return f'{self.title}'
+# Exercise:
+# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
+# make coffee/wifi/power a select element with choice of 0 to 5.
+#e.g. You could use emojis ☕️/💪/✘/🔌
+# make all fields required except submit
+# use a validator to check that the URL field has a URL entered.
+# ---------------------------------------------------------------------------
 
 
-with app.app_context():
-    db.create_all()
-    new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
-    db.session.add(new_book)
-
-
-with app.app_context():
-    db.session.commit()
-    books = Book.query.all()
-
-for book in books:
-    print(book.title)
-    print(book.author)
-
-
-
-# db = sqlite3.connect('books_collection.db')
-# cursor = db.cursor()
-
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,"
-# "author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
-
-# cursor.execute("INSERT INTO Books VALUES(1, 'Harry Porter','jk Rolling', '9.3')")
-# db.commit()
-
-
-@app.route('/')
+# all Flask routes below
+@app.route("/")
 def home():
-    all_books = db.session.query(Book).all()
-    return render_template("index.html", books=all_books)
+    return render_template("index.html")
 
 
-@app.route("/add", methods=["POST", "GET"])
-def add():
-    if request.method == "POST":
-        new_book =Book (
-            title= request.form["title"],
-            author= request.form["author"],
-            rating= request.form["rating"]
-        )
-        db.session.add(new_book)
-        db.session.commit()
-    return render_template("add.html")
-    return redirect(url_for("home"))
+@app.route('/add')
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        print("True")
+    # Exercise:
+    # Make the form write a new row into cafe-data.csv
+    # with   if form.validate_on_submit()
+    return render_template('add.html', form=form)
 
 
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
 
 
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5002)
+if __name__ == '__main__':
+    app.run(debug=True)
